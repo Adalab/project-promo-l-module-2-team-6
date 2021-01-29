a7eee99066703ee7758b97b023e3257d4ec329cb
cvcastano <carocastanocastillo@gmail.com>
2021-01-29 12:57:29 +0100

    merge design branch

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// console.log('>> Ready :)');
+console.log('>> Ready :)');
 
-// const inputName = document.querySelector('.js-input-name');
-// const previewName = document.querySelector('.js-preview-name');
+const inputName = document.querySelector('.js-input-name');
+const previewName = document.querySelector('.js-preview-name');
 
-// function pickUpInput () {
-//     previewName.innerHTML = event.currentTarget.value;
-// }
-// inputName.addEventListener('keyup', pickUpInput);
+function pickUpInput () {
+    previewName.innerHTML = event.currentTarget.value;
+}
+inputName.addEventListener('keyup', pickUpInput);
