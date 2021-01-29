616288423b34b5e734e0c6e0db1cae294325aeed
cvcastano <carocastanocastillo@gmail.com>
2021-01-29 13:09:09 +0100

    add jsfolders

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,2 @@
 'use strict';
 
-console.log('>> Ready :)');
-
-const inputName = document.querySelector('.js-input-name');
-const previewName = document.querySelector('.js-preview-name');
-
-function pickUpInput () {
-    previewName.innerHTML = event.currentTarget.value;
-}
-inputName.addEventListener('keyup', pickUpInput);
