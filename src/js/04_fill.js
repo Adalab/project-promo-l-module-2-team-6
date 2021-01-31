5877310089debb8b26f57c47c456c6c5adc4f0b9
vtendero <tendero.valle@gmail.com>
2021-01-31 19:41:33 +0100

    add handlerJob function

--- src/js/04_fill.js
+++ src/js/04_fill.js
@@ -4,8 +4,16 @@ console.log('>> Ready :)');
 
 const inputName = document.querySelector('.js-input-name');
 const previewName = document.querySelector('.js-preview-name');
+const jobElement = document.querySelector('.js-input-job');
+const jobPreviewElement = document.querySelector('.js-preview-job');
 
 function pickUpInput () {
     previewName.innerHTML = event.currentTarget.value;
 }
 inputName.addEventListener('keyup', pickUpInput);
+
+
+function handlerJob () {
+    jobPreviewElement.innerHTML = event.currentTarget.value;
+}
+jobElement.addEventListener('keyup', handlerJob);
