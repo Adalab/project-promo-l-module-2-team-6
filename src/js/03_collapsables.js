1fd8384ad0c9c5e696adec87716765174a0bd729
jechfernandez <jechfernandez@gmail.com>
2021-02-01 18:37:52 +0100

    Check collapsables are working

--- src/js/03_collapsables.js
+++ src/js/03_collapsables.js
@@ -1,19 +1,31 @@
 'use strict';
 
 //Elemento HTML
-const designCollapsableHeader=document.querySelector('.js-collapsable-header');
-const designCollapsableCont=document.querySelector('.js-collapsable-container');
-const arrow = document.querySelector('.js-arrow-up');
+const collapsableHeader=document.querySelector('.js-collapsable-header');
+const collapsableContainer=document.querySelector('.js-collapsable-container');
+const arrowButton=document.querySelector('.js-arrow-up');
+
+// const designCollapsableHeader=document.querySelector('.js-collapsable-header');
+// const designCollapsableCont=document.querySelector('.js-collapsable-container');
+// const arrow = document.querySelector('.js-arrow-up');
+
+// FUNCIÓN
+function handleCollapsable(){
+    collapsableContainer.classList.toggle('collapsable-container');
+    arrowButton.classList.toggle('js-design-arrow');
+    console.log('Hice click');
+};
+
+//EVENTO
+collapsableHeader.addEventListener('click', handleCollapsable);
+
+// function handleCollapsable() {
+//     designCollapsableCont.classList.toggle('designCollapsableCont');
+//     arrow.classList.toggle('js-design-arrow');
+//     console.log('Click aquí')
+// }
 
 
 
-function handleCollapsable() {
-    designCollapsableCont.classList.toggle('designCollapsableCont');
-    arrow.classList.toggle('js-design-arrow');
-    console.log('Click aquí')
-}
-
-
-
-designCollapsableHeader.addEventListener('click', handleCollapsable);
+// designCollapsableHeader.addEventListener('click', handleCollapsable);
 
