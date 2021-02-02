64863d1e794d6be94115e342e138c1ca7229715a
jechfernandez <jechfernandez@gmail.com>
2021-02-02 09:37:24 +0100

    Save background img in images folder

--- src/js/05_reset.js
+++ src/js/05_reset.js
@@ -1,5 +1,12 @@
+'use strict'
 // ELEMENTOS HTML
 const reset = document.querySelector(".js-reset");
 const paletteReset = document.querySelector(".js-palette");
 
 //FUNCIÓN RESET
+function resetCard() {
+    
+  }
+  
+  //EVENTO
+  reset.addEventListener("click", resetCard);
