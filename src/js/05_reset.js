e22e166df531e8d5ae9b21fc73feed21658bb8c2
jechfernandez <jechfernandez@gmail.com>
2021-02-04 08:33:35 +0100

    Attempt to create reset button

--- src/js/05_reset.js
+++ src/js/05_reset.js
@@ -1,12 +1,36 @@
 'use strict'
-// ELEMENTOS HTML
-const reset = document.querySelector(".js-reset");
-const paletteReset = document.querySelector(".js-palette");
 
-//FUNCIÓN RESET
-function resetCard() {
-    
-  }
+const resetButton = document.querySelector(".js-reset");
+
+const resetLocalStorage = () => {
+  localStorage.clear('js-palette');
+  localStorage.clear('js-preview-name');
+  localStorage.clear('js-preview-job');
+  localStorage.clear('image');
+  localStorage.clear('phone');
+  localStorage.clear('email');
+  localStorage.clear('linkedin');
+  localStorage.clear('github');
+};
+
+const resetName = () => {
+  previewCardName.innerHTML = 'Greta Thunberg';
+  userName.value = '';
+};
+
+    const resetJob = () => {
+      previewCardJob.innerHTML = 'Activista';
+  userJob.value = '';
+};
   
-  //EVENTO
-  reset.addEventListener("click", resetCard);
+
+const resetCard = () => {
+  resetLocalStorage();
+  resetColorPalette();
+  resetFont();
+  resetName();
+  resetJob();
+};
+
+// EVENTO
+resetButton.addEventListener('click', resetCard);
